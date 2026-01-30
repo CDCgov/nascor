f8aca1e1c7a26f736be1c83724167c104010fb66
Scott Olesen <ulp7@cdc.gov>
2026-01-30 17:52:07 -0500

    Parsing, tiling, docs (#1)

--- docs/index.md
+++ docs/index.md
@@ -1,5 +1,13 @@
 # nascor
 
-Tools for manipulating ages and age ranges.
+nascor is a tool for manipulating ages and age ranges. The name is from [the Latin](https://en.wiktionary.org/wiki/nascor#Latin).
 
-The name is from [the Latin](https://en.wiktionary.org/wiki/nascor#Latin).
+In nascor, each _age_ is a period of time, from age zero (i.e., birth) to some later time, defined in years, months, or weeks. We can easily compare ages of the same unit, but in general, we cannot compare ages of different units:
+
+- One year equals 12 months.
+- Zero of anything equals zero of anything else.
+- Otherwise, there is no comparison: a month is not 4 weeks, nor is a year 52 weeks.
+
+An _age range_ is a pair of ages (like 0-18 years) or a single age with no upper limit (like 65+ years). nascor can parse certain string representations of age ranges.
+
+Note that "age" is often loosely used to refer to an age _range_. For example, "1 year olds" are those individuals between 1.0 and 2.0 years of age. This distinction helps clarify that "0-<1 years" refers to 0.0 to 1.0 years while "0-1 years" refers to individuals from 0.0 to 2.0 years.
--- nascor/__init__.py
+++ nascor/__init__.py
@@ -1,75 +1,187 @@
 import functools
 import itertools
-from typing import Literal, Self, Tuple
+import re
+from typing import Iterable, Literal, Self, Tuple
+
+Unit = Literal["year", "month", "week"]
 
 
 @functools.total_ordering
 class Age:
-    def __init__(self, x: int, metric: Literal["year", "month", "week"]):
+    def __init__(self, n: int, unit: Unit):
         """
         An age is a duration of time
 
         Args:
-            x: number of units of time
-            metric: unit
+            n: number of units of time
+            unit: one of `"year"`, `"month"`, or `"week"`
         """
-        self.x = x
-        self.metric = metric
+        assert n >= 0
+        self.n = n
+        self.unit = unit
 
     def _get_cmp(self, other: Self) -> Tuple[int, int]:
-        if self.metric == other.metric:
-            return (self.x, other.x)
-        elif self.x == 0 and other.x == 0:
+        """
+        To enable comparison of ages, convert, where possible, into a
+        pair of values in the the same units.
+        """
+        if self.unit == other.unit:
+            return (self.n, other.n)
+        elif self.n == 0 and other.n == 0:
             return (0, 0)
-        elif self.metric == "year" and other.metric == "month":
-            return (self.x * 12, other.x)
-        elif self.metric == "month" and other.metric == "year":
-            return (self.x, other.x * 12)
+        elif self.unit == "year" and other.unit == "month":
+            return (self.n * 12, other.n)
+        elif self.unit == "month" and other.unit == "year":
+            return (self.n, other.n * 12)
         else:
             raise ValueError(f"Cannot compare {self} and {other}")
 
     def __eq__(self, other: Self) -> bool:
-        x1, x2 = self._get_cmp(other)
-        return x1 == x2
+        n1, n2 = self._get_cmp(other)
+        return n1 == n2
 
     def __lt__(self, other: Self) -> bool:
-        x1, x2 = self._get_cmp(other)
-        return x1 < x2
+        if self.n == 0 and other.n > 0:
+            return True
+        else:
+            n1, n2 = self._get_cmp(other)
+            return n1 < n2
 
     def __repr__(self) -> str:
-        return f"Age(x={self.x}, metric='{self.metric}')"
+        return f"Age(n={self.n}, unit='{self.unit}')"
 
     def __hash__(self) -> int:
-        return hash((self.x, self.metric))
+        return hash((self.n, self.unit))
 
 
-class AgeGroup:
-    def __init__(self, start: Age, end: Age):
+class AgeRange:
+    def __init__(self, lower: Age, upper: Age | None):
         """
-        An age group is an ordered pair of ages
+        An age range is an ordered pair of ages
 
         Args:
-            start: lower end of the age range
-            end: upper end of the range
+            lower: lower limit of the age range
+            upper: upper limit of the range. `None` means no upper limit.
         """
-        assert start < end
-        self.start = start
-        self.end = end
+        if upper is not None:
+            assert lower < upper
+
+        self.lower = lower
+        self.upper = upper
 
     def is_in(self, other: Self) -> bool:
         """Is this range inside another range?"""
-        return other.start <= self.start and self.end <= other.end
+        return other.lower <= self.lower and (
+            # upper infinite ranges are OK
+            (self.upper is None and other.upper is None)
+            # finite fits inside infinite
+            or (self.upper is not None and other.upper is None)
+            or (
+                # if both are finite, do the normal comparison
+                self.upper is not None
+                and other.upper is not None
+                and self.upper <= other.upper
+            )
+        )
 
     def subdivide(self, cuts: list[Age]) -> list[Self]:
         """Break this range down into smaller ranges"""
         assert len(cuts) >= 2
-        assert cuts[0] == self.start
-        assert cuts[-1] == self.end
+        assert cuts[0] == self.lower
+        assert cuts[-1] == self.upper
 
-        return [type(self)(start, end) for start, end in itertools.pairwise(cuts)]
+        return [type(self)(lower, upper) for lower, upper in itertools.pairwise(cuts)]
 
     def __repr__(self) -> str:
-        return f"AgeGroup({self.start}, {self.end})"
+        return f"AgeRange({self.lower}, {self.upper})"
 
     def __eq__(self, other: Self) -> bool:
-        return self.start == other.start and self.end == other.end
+        return self.lower == other.lower and self.upper == other.upper
+
+
+"""
+The master list of age ranges we can parse.
+
+Each element is a tuple of
+- A regex which can extract the single age or the lower/upper ages and
+- A Callable which returns an AgeRange
+"""
+PARSERS = (
+    (
+        re.compile(r"^(\d+) years?$"),
+        lambda x: AgeRange(Age(int(x[0]), "year"), Age(int(x[0]) + 1, "year")),
+    ),
+    (
+        re.compile(r"^(\d+)\+ years?$"),
+        lambda x: AgeRange(Age(int(x[0]), "year"), None),
+    ),
+    (
+        re.compile(r"^(\d+)-(\d+) years?$"),
+        lambda x: AgeRange(Age(x[0], "year"), Age(int(x[1]) + 1, "year")),
+    ),
+    (
+        re.compile(r"^(\d+)-<(\d+) years?$"),
+        lambda x: AgeRange(Age(int(x[0]), "year"), Age(int(x[1]), "year")),
+    ),
+    (
+        re.compile(r"^(\d+) months?-(\d+) years?$"),
+        lambda x: AgeRange(Age(int(x[0]), "month"), Age(int(x[1]) + 1, "year")),
+    ),
+    (
+        re.compile(r"^(\d+) months?-<(\d+) years?$"),
+        lambda x: AgeRange(Age(int(x[0]), "month"), Age(int(x[1]), "year")),
+    ),
+    (
+        re.compile(r"^(\d+)-(\d+) months?$"),
+        lambda x: AgeRange(Age(int(x[0]), "month"), Age(int(x[1]) + 1, "month")),
+    ),
+    (
+        re.compile(r"^(\d+)-<(\d+) months?$"),
+        lambda x: AgeRange(Age(int(x[0]), "month"), Age(int(x[1]), "month")),
+    ),
+)
+
+
+class ParseFailure:
+    def __init__(self, string: str):
+        self.string = string
+
+    def __str__(self):
+        return f"Failured to parse age range: '{self.string}'"
+
+
+def parse_range(x: str) -> AgeRange | ParseFailure:
+    """
+    Parse an age range string
+
+    Args:
+        x: string
+
+    Returns: an `AgeRange`, or a `ParseFailure`
+    """
+    for regex, value_fun in PARSERS:
+        if match := regex.fullmatch(x):
+            return value_fun(match.groups())
+
+    return ParseFailure(x)
+
+
+def tile(parts: Iterable[AgeRange], whole: AgeRange) -> bool:
+    """
+    Does a set of `AgeRange`s cover another `AgeRange`, with no gaps or overlap?
+
+    Args:
+        parts: member `AgeRange`s
+        whole: larger `AgeRange`
+
+    Returns: boolean
+    """
+    parts = sorted(parts, key=lambda x: x.lower)
+
+    lower_match = parts[0].lower == whole.lower
+    upper_match = parts[-1].upper == whole.upper
+    middle_matches = [
+        parts[i].upper == parts[i + 1].lower for i in range(len(parts) - 1)
+    ]
+
+    return lower_match and upper_match and all(middle_matches)
